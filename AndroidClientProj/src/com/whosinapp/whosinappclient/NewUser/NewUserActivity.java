8ebddd7f0e77ca365cf96655f4e7df787ff2bb09
Guillermo Llosa <GWLlosa@gmail.com>
2012-09-08 02:24:59 -0400

    Its done.

--- AndroidClientProj/src/com/whosinapp/whosinappclient/NewUser/NewUserActivity.java
+++ AndroidClientProj/src/com/whosinapp/whosinappclient/NewUser/NewUserActivity.java
@@ -7,13 +7,22 @@ import com.whosinapp.whosinappclient.R.menu;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class NewUserActivity extends Activity {
-
+public class NewUserActivity extends Activity
+implements OnClickListener
+{
+	NewUserActivityController controller = new NewUserActivityController(this);
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+        Button submitButton = (Button)this.findViewById(R.id.button_Submit);
+        submitButton.setOnClickListener(this);
     }
 
     @Override
@@ -21,4 +30,20 @@ public class NewUserActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_new_user, menu);
         return true;
     }
+
+    private String getTextFromEditViewById(int id)
+    {
+    	EditText theEditView = (EditText)this.findViewById(id);
+    	return theEditView.getText().toString();
+    }
+	public void onClick(View arg0) {
+		String FirstName = getTextFromEditViewById(R.id.editText_firstName);
+		
+		String LastName= getTextFromEditViewById(R.id.editText_LastName);
+		String EmailAddress= getTextFromEditViewById(R.id.editText_Email);
+		String Password= getTextFromEditViewById(R.id.editText_password);
+		String PasswordRepeat= getTextFromEditViewById(R.id.editText_passConfirm);
+		
+		
+	}
 }
--- AndroidClientProj/src/com/whosinapp/whosinappclient/NewUser/NewUserActivityController.java
+++ AndroidClientProj/src/com/whosinapp/whosinappclient/NewUser/NewUserActivityController.java
@@ -1,9 +1,37 @@
 package com.whosinapp.whosinappclient.NewUser;
 
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+
+import com.whosinapp.whosinappclient.ServiceGateway;
+
 public class NewUserActivityController {
 
 	public NewUserActivityController(NewUserActivity newUserActivity) {
 		// TODO Auto-generated constructor stub
+	}
+
+	public void SubmitNewUser(String firstName, String lastName,
+			String emailAddress, String password, String passwordRepeat) {
+		NewUserRequestDto dto = new NewUserRequestDto();
+		dto.setEmailAddress(emailAddress);
+		dto.setFirstName(firstName);
+		dto.setLastName(lastName);
+		dto.setPassword(password);
+		dto.setPasswordConfirmation(passwordRepeat);
+		
+		ServiceGateway gate = new ServiceGateway();
+		try {
+			gate.Send(dto);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
