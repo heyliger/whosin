619e8ffe89de4a0c1d6a3d1590c6e1cab4cd949d
yoel <gonzalezyoel@bellsouth.net>
2012-09-07 23:12:49 -0400

    Completed LoginRequestDto

--- AndroidClientProj/src/com/whosinapp/whosinappclient/LoginRequestDto.java
+++ AndroidClientProj/src/com/whosinapp/whosinappclient/LoginRequestDto.java
@@ -1,5 +1,26 @@
 package com.whosinapp.whosinappclient;
 
 public class LoginRequestDto {
-
-}
+	private String userName;
+	private String passWord;
+	
+	public String getPassword()
+	{
+		return passWord;
+	}
+	
+	public void setPassword(String value)
+	{
+		passWord = value;
+	}
+	
+	public String getUserName()
+	{
+		return userName;
+	}
+	
+	public void setUserName(String value)
+	{
+		userName = value;
+	}
+} 
