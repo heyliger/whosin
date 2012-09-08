7bebb5870dae97f18cea19be94446716c1a27ee9
Guillermo Llosa <GWLlosa@gmail.com>
2012-09-07 23:10:30 -0400

    Eventing
    Its in

--- AndroidClientProj/src/com/whosinapp/whosinappclient/MainActivity.java
+++ AndroidClientProj/src/com/whosinapp/whosinappclient/MainActivity.java
@@ -2,14 +2,24 @@ package com.whosinapp.whosinappclient;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity
+implements OnClickListener
+{
+	MainActivityController myController = new MainActivityController(this);
+	Button doLogin ;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        doLogin = (Button) this.findViewById( R.id.button1);
+        doLogin.setClickable(true);
+        doLogin.setOnClickListener((android.view.View.OnClickListener) this);
     }
 
     @Override
@@ -17,4 +27,12 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+
+	public void onClick(DialogInterface arg0, int arg1) {
+		// TODO Auto-generated method stub
+		EditText userName = (EditText) this.findViewById(R.id.editText1);
+		EditText passWord = (EditText) this.findViewById(R.id.editText2);
+		
+		myController.DoLogin(userName.getText(),passWord.getText());
+	}
 }
--- AndroidClientProj/src/com/whosinapp/whosinappclient/MainActivityController.java
+++ AndroidClientProj/src/com/whosinapp/whosinappclient/MainActivityController.java
@@ -1,5 +1,18 @@
 package com.whosinapp.whosinappclient;
 
+import android.text.Editable;
+
 public class MainActivityController {
+
+	public MainActivityController(MainActivity mainActivity) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void DoLogin(Editable text, Editable text2) {
+		// TODO Auto-generated method stub
+		String user = text.toString();
+		String pass = text2.toString();
+		
+	}
 
 }
